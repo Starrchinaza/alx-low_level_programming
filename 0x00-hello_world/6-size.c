d623efb4465a050317c006dd2d34ed50ba5e36ce
Starrchinaza <Starrchinazalordsweetness@icloud.com>
2023-02-16 19:24:31 -0800

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	printf("size of char :%i. \n", sizeof(char));
+	printf("size of int :%i. \n", sizeof(int));
+	printf("size of long int :%i. \n", sizeof(long int));
+	printf("size of long long int :%i. \n", sizeof(long long int));
+	printf("size of float :%i. \n", sizeof(float));
+	return (0);
+}
