98759f79d34f20c1e078e1ecb257a41d7477ca84
Starrchinaza <Starrchinazalordsweetness@icloud.com>
2023-02-27 19:48:37 -0800

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - This function updates value
+* @n: This parameter updating
+* Return: void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - this function swap value
+* @a: parameter 1
+* @b: parameter 2
+* Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	int f;
+
+	f = *a;
+	*a = *b;
+	*b = f;
+}
