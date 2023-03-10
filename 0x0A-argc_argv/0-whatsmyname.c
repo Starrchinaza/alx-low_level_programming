26de7be7878fc44a3fb71c8ff9f7098b952acdb7
Starrchinaza <Starrchinazalordsweetness@icloud.com>
2023-03-10 15:26:00 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the function
+ * @argc: the count
+ * @argv: the array of string
+ * Return: 0 always
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the function
+ * @argc: the count
+ * @argv: the array of string
+ * Return: 0 always
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
